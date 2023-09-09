751d5b99788d7ab6956d343e31ef9586004ced8d
KennedyMelo <kennedycmelo@gmail.com>
2023-09-09 17:12:44 -0300

    feat: add create user

--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,3 +1,5 @@
+import prismaClient from '../../prisma'
+
 interface UserRequest{
   name: string;
   email: string;
@@ -6,7 +8,37 @@ interface UserRequest{
 
 class CreateUserService{
   async execute({name, email, password}: UserRequest){
-    return { name}
+
+    //check if he sended an email
+    if(!email){
+      throw new Error("Email incorrect")
+    }
+
+    //Check if the email already exists
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: {
+        email: email
+      }
+    })
+
+    if(userAlreadyExists){
+      throw new Error("User already exists")
+    }
+
+    const user = await prismaClient.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: password
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true
+      }
+    })
+    
+    return user;
   }
 }
 
