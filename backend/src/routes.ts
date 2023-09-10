d7f6736dc89e462b039f50c69127dc47f86d7f87
KennedyMelo <kennedycmelo@gmail.com>
2023-09-10 16:22:30 -0300

    feat: add create category

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,6 +5,7 @@ import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
+import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 
 const router = Router();
 
@@ -14,4 +15,8 @@ router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 
 router.get('/me', isAuthenticated, new DetailUserController().handle)
+
+//-- Rotas Category
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+
 export {router};
