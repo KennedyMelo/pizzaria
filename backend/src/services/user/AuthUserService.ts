e9dba7b544ccae9d96a5a522052becc7e4225148
KennedyMelo <kennedycmelo@gmail.com>
2023-09-10 15:16:27 -0300

    feat: add jwt

--- backend/src/services/user/AuthUserService.ts
+++ backend/src/services/user/AuthUserService.ts
@@ -1,5 +1,6 @@
 import prismaClient from "../../prisma";
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 interface AuthRequest{
   email: string;
@@ -27,8 +28,23 @@ class AuthUserService{
     }
 
     // generate JWT e return the user data as id, name and email 
-
-    return {ok: true}
+    const token = sign(
+      {
+        name: user.name,
+        email: user.email
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: '30d'
+      }
+    )
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      token
+    }
   }
 }
 
