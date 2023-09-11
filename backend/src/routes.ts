d40c36295c23dc4ae45f37a8dc7ba284ff93d887
KennedyMelo <kennedycmelo@gmail.com>
2023-09-11 11:52:32 -0300

    feat: add list categories

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,7 +5,10 @@ import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
+
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
+import { ListCategoryController } from './controllers/category/ListCategoryController';
+
 
 const router = Router();
 
@@ -19,4 +22,9 @@ router.get('/me', isAuthenticated, new DetailUserController().handle)
 //-- Rotas Category
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 
-export {router};
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
+
+
+
+
+export { router };
